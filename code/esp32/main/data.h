ee1145e9fe2e309290002618b2c0bcd328ad7526
Levi Harrison <git@leviharrison.dev>
2025-10-28 10:16:25 -0400

    document data format

--- code/esp32/main/data.h
+++ code/esp32/main/data.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 
+// Sensor readings taken at predetermined intervals.
 typedef struct
 {
     float depth[360];
@@ -10,14 +11,28 @@ typedef struct
 
 typedef struct
 {
+    // sequence_id is the sequence of packets this packet belong to.
+    // Each sequence is all the data for a period of time, probably
+    // a day.
     uint16_t sequence_id;
+
+    // packet_num is the number of this packet in the sequence.
     uint8_t packet_num;
+
+    // total is the total number of packets in the sequence.
     uint16_t total;
+
+    // timestamp is the time of the start of the data in this packet.
     uint64_t timestamp;
+
+    // sensor_id is the sensor this packet is coming from.
     uint16_t sensor_id;
+
+    // payload is the data.
     data payload;
 } packet_t;
 
-typedef struct {
+typedef struct
+{
     uint64_t timestamp;
 } time_sync_packet_t;
