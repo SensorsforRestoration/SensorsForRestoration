bc4ee64bb56604db5f2861480b017aee5cac1def
keyichen29 <keyichen@college.harvard.edu>
2025-11-11 09:29:06 -0500

    V1 Superior Depth Sensor Code

---
+++ code/esp32/Superior_Depth_Code_1.ino
@@ -0,0 +1,52 @@
+unsigned char buffer_RTT[4] = {0};
+uint8_t CS;
+
+
+//defines pins and BAUD rate
+#define COM 0x55
+#define RXD2 16
+#define TXD2 17
+#define BAUD 115200
+
+//intializes class
+HardwareSerial sillySerial(2);
+
+
+int Distance = 0;
+void setup() {
+
+  Serial.begin(115200);
+  sillySerial.begin(BAUD, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("Serial 2 started at 9600 baud rate");
+
+}
+void loop() {
+  sillySerial.write(COM);
+  delay(100);
+
+  if(sillySerial.available() > 0){
+    Serial.println("available");
+    delay(4);
+    
+    if(sillySerial.read() == 0xff){    
+      buffer_RTT[0] = 0xff;
+      Serial.println("first bit there");
+
+      for (int i=1; i<4; i++){
+        buffer_RTT[i] = sillySerial.read();
+        Serial.println(buffer_RTT[i]);
+      }
+      CS = buffer_RTT[0] + buffer_RTT[1]+ buffer_RTT[2];  
+
+      if(buffer_RTT[3] == CS) {
+        Distance = (buffer_RTT[1] << 8) + buffer_RTT[2];
+        Serial.print("Distance:");
+        Serial.print(Distance);
+        Serial.println("mm");
+      }
+    }
+  }
+  else{
+    Serial.println("not available");
+  }
+}
